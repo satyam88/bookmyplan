8c914772d7c9b5ed82bb541235c8c954c48e56b8
“satyam88git config --global user.email satyam@gmail.com <satyampandey@192.168.1.4>
2026-01-22 10:25:25 +0530

    adding new javaclass

---
+++ src/main/java/com/example/bookmyplan/flight.java
@@ -0,0 +1,13 @@
+package com.example.bookmyplan;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class flight {
+
+    @GetMapping("/flight")
+    public String getData() {
+        return "Hello Welcome to BookMyPlan, Please book flight tickets at 10% discount";
+    }
+}
---
+++ src/main/java/com/example/bookmyplan/hotel.java
@@ -0,0 +1,13 @@
+package com.example.bookmyplan;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class hotel {
+
+    @GetMapping("/hotel")
+    public String getData() {
+        return "Hello Welcome to BookMyPlan, Please book hotel tickets at 10% discount.";
+    }
+}
