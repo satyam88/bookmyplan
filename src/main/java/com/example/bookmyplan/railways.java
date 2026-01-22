65b49e8892688bb3c0e9649167649fb1691d9b86
“satyam88git config --global user.email satyam@gmail.com <satyampandey@192.168.1.4>
2026-01-22 10:37:01 +0530

    adding railws

---
+++ src/main/java/com/example/bookmyplan/railways.java
@@ -0,0 +1,13 @@
+package com.example.bookmyplan;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class railways {
+
+    @GetMapping("/railways")
+    public String getData() {
+        return "Hello Welcome to BookMyPlan, Please book railways tickets at 10% discount";
+    }
+}
