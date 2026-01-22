6d60808100e9497799293fcc1f7084392891bbe3
“satyam88git config --global user.email satyam@gmail.com <satyampandey@192.168.1.4>
2026-01-22 09:59:01 +0530

    coderefactor

---
+++ src/main/java/com/example/bookmyplan/BookmyplanApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookmyplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmyplanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmyplanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bookmyplan/BookmyplanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookmyplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmyplanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
