5706d463cd5be01745e703b971b34287feb6ac47
satyam88 <satyampandey88@gmail.com>
2026-01-27 11:00:57 +0530

    adding newchanges

--- src/main/java/com/example/bookmyplan/railwayscontroller.java
+++ src/main/java/com/example/bookmyplan/railwayscontroller.java
@@ -14,15 +14,5 @@ public class railwayscontroller {
     @GetMapping("/superfast-railways")
     public String getLocalRailways() {
         return "Hello Welcome to BookMyPlan, Please book ddddddd railways tickets at 30% discount";
-
-    @GetMapping("/pre-railways")
-    public String getLocalRailways() {
-        return "Hello Welcome to BookMyPlan, sddsd book pre railways tickets at 30% discount";
-        
-    }
-
-    @GetMapping("/goods-railways")
-    public String getIocalRailways() {
-        return "Hello Welcome to BookMyPlan, Please book goods ddddddddd tickets at 30% discount";
     }
 }
