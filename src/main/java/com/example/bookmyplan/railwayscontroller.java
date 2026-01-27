77e89b44ea7a65d8e18a9e48410cc6e25e4b845c
satyam88 <satyampandey88@gmail.com>
2026-01-27 10:30:45 +0530

    randome

--- src/main/java/com/example/bookmyplan/railwayscontroller.java
+++ src/main/java/com/example/bookmyplan/railwayscontroller.java
@@ -8,21 +8,21 @@ public class railwayscontroller {
 
     @GetMapping("/railways")
     public String getIndianRailways() {
-        return "Hello Welcome to BookMyPlan, Please book Indian Railways tickets at 60% discount";
+        return "Hello Welcome to BookMyPlan, Please book Indian dddddddd tickets at 60% discount";
     }
 
     @GetMapping("/superfast-railways")
     public String getLocalRailways() {
-        return "Hello Welcome to BookMyPlan, Please book superfast railways tickets at 30% discount";
-        
+        return "Hello Welcome to BookMyPlan, Please book ddddddd railways tickets at 30% discount";
+
     @GetMapping("/pre-railways")
     public String getLocalRailways() {
-        return "Hello Welcome to BookMyPlan, Please book pre railways tickets at 30% discount";        
+        return "Hello Welcome to BookMyPlan, sddsd book pre railways tickets at 30% discount";
         
     }
 
     @GetMapping("/goods-railways")
     public String getIocalRailways() {
-        return "Hello Welcome to BookMyPlan, Please book goods railways tickets at 30% discount";
+        return "Hello Welcome to BookMyPlan, Please book goods ddddddddd tickets at 30% discount";
     }
 }
