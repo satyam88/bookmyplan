da3cb12f6b6098408cdcdec367b84a606ea80f65
satyam88 <satyampandey88@gmail.com>
2026-01-27 10:16:58 +0530

    hello

---
+++ src/main/java/com/example/bookmyplan/railwayscontroller.java
@@ -0,0 +1,18 @@
+package com.example.bookmyplan;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class railwayscontroller {
+
+    @GetMapping("/railways")
+    public String getIndianRailways() {
+        return "Hello Welcome to BookMyPlan, Please book Indian Railways tickets at 50% discount";
+    }
+
+    @GetMapping("/local-railways")
+    public String getLocalRailways() {
+        return "Hello Welcome to BookMyPlan, Please book local railways tickets at 20% discount";
+    }
+}
