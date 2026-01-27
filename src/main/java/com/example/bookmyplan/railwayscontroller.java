6dce7a047c8ade83cf81452abac13865259bac56
satyam88 <satyampandey88@gmail.com>
2026-01-27 11:03:21 +0530

    Change endpoint from '/railways' to '/one-railways'

--- src/main/java/com/example/bookmyplan/railwayscontroller.java
+++ src/main/java/com/example/bookmyplan/railwayscontroller.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class railwayscontroller {
 
-    @GetMapping("/railways")
+    @GetMapping("/one-railways")
     public String getIndianRailways() {
         return "Hello Welcome to BookMyPlan, Please book Indian dddddddd tickets at 60% discount";
     }
