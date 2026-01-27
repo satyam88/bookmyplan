6ca28c87bf24a6125ccd3036b916c0ec3db000a4
satyam88 <satyampandey88@gmail.com>
2026-01-27 10:52:24 +0530

    hell

--- src/main/java/com/example/bookmyplan/railwayscontroller.java
+++ src/main/java/com/example/bookmyplan/railwayscontroller.java
@@ -14,5 +14,79 @@ public class railwayscontroller {
     @GetMapping("/superfast-railways")
     public String getSuperfastRailways() {
         return "Hello Welcome to BookMyPlan, Please book superfast railways tickets at 30% discount";
+
+
+        @GetMapping("/railways")
+        public String getIndianRailways() {
+            return "Hello Welcome to BookMyPlan, Please book Indian Railways tickets at 60% discount";
+        }
+
+        @GetMapping("/superfast-railways")
+        public String getSuperfastRailways() {
+            return "Hello Welcome to BookMyPlan, Please book superfast railways tickets at 30% discount";
+            @GetMapping("/railways")
+            public String getIndianRailways() {
+                return "Hello Welcome to BookMyPlan, Please book Indian Railways tickets at 60% discount";
+            }
+
+            @GetMapping("/superfast-railways")
+            public String getSuperfastRailways() {
+                return "Hello Welcome to BookMyPlan, Please book superfast railways tickets at 30% discount";    @GetMapping("/railways")
+                public String getIndianRailways() {
+                    return "Hello Welcome to BookMyPlan, Please book Indian Railways tickets at 60% discount";
+                }
+
+                @GetMapping("/superfast-railways")
+                public String getSuperfastRailways() {
+                    return "Hello Welcome to BookMyPlan, Please book superfast railways tickets at 30% discount";    @GetMapping("/railways")
+                    public String getIndianRailways() {
+                        return "Hello Welcome to BookMyPlan, Please book Indian Railways tickets at 60% discount";
+                    }
+
+                    @GetMapping("/superfast-railways")
+                    public String getSuperfastRailways() {
+                        return "Hello Welcome to BookMyPlan, Please book superfast railways tickets at 30% discount";    @GetMapping("/railways")
+                        public String getIndianRailways() {
+                            return "Hello Welcome to BookMyPlan, Please book Indian Railways tickets at 60% discount";
+                        }
+
+                        @GetMapping("/superfast-railways")
+                        public String getSuperfastRailways() {
+                            return "Hello Welcome to BookMyPlan, Please book superfast railways tickets at 30% discount";    @GetMapping("/railways")
+                            public String getIndianRailways() {
+                                return "Hello Welcome to BookMyPlan, Please book Indian Railways tickets at 60% discount";
+                            }
+
+                            @GetMapping("/superfast-railways")
+                            public String getSuperfastRailways() {
+                                return "Hello Welcome to BookMyPlan, Please book superfast railways tickets at 30% discount";    @GetMapping("/railways")
+                                public String getIndianRailways() {
+                                    return "Hello Welcome to BookMyPlan, Please book Indian Railways tickets at 60% discount";
+                                }
+
+                                @GetMapping("/superfast-railways")
+                                public String getSuperfastRailways() {
+                                    return "Hello Welcome to BookMyPlan, Please book superfast railways tickets at 30% discount";    @GetMapping("/railways")
+                                    public String getIndianRailways() {
+                                        return "Hello Welcome to BookMyPlan, Please book Indian Railways tickets at 60% discount";
+                                    }
+
+                                    @GetMapping("/superfast-railways")
+                                    public String getSuperfastRailways() {
+                                        return "Hello Welcome to BookMyPlan, Please book superfast railways tickets at 30% discount";    @GetMapping("/railways")
+                                        public String getIndianRailways() {
+                                            return "Hello Welcome to BookMyPlan, Please book Indian Railways tickets at 60% discount";
+                                        }
+
+                                        @GetMapping("/superfast-railways")
+                                        public String getSuperfastRailways() {
+                                            return "Hello Welcome to BookMyPlan, Please book superfast railways tickets at 30% discount";
+
+
+
+
+
+
+
     }
 }
